e3ce79f2447c10fbeb99e350e544de4ebcc56431
Sabina Omarova <sabomar170@gmail.com>
2022-10-19 23:53:37 -0400

    Lab 00- coupling - loosely

---
+++ lab-00-coupling/src/main/java/com/cydeo/loosely/Balance.java
@@ -0,0 +1,17 @@
+package com.cydeo.loosely;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+public abstract class Balance {
+    private UUID userId;
+    private BigDecimal amount;
+
+    public abstract BigDecimal addBalance(BigDecimal amount);
+
+}
---
+++ lab-00-coupling/src/main/java/com/cydeo/loosely/BalanceManager.java
@@ -0,0 +1,12 @@
+package com.cydeo.loosely;
+
+import java.math.BigDecimal;
+
+public class BalanceManager {
+ public boolean checkout(Balance balance, BigDecimal amount){
+        BigDecimal balanceAmount = balance.getAmount();
+
+        return balanceAmount.subtract(amount)
+                .compareTo(BigDecimal.ZERO) > 0;
+    }
+}
