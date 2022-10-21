22620ff07aeaf8730e4f35a9c7f634ef1887e7bc
Sabina Omarova <sabomar170@gmail.com>
2022-10-21 11:39:48 -0400

    Lab 02- di

---
+++ lab-02-di/src/main/java/com/cydeo/service/StockServiceImpl.java
@@ -0,0 +1,14 @@
+
+
+package com.cydeo.service;
+
+import com.cydeo.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockServiceImpl implements StockService{
+    @Override
+    public boolean checkStockIsAvailable(Product product, int quantity) {
+        return product.getRemainingQuantity() > quantity;
+    }
+}
