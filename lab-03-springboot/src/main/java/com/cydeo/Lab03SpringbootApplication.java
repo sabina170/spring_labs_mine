83cb2f1f790439d1f3fc16c95db6c1f2d7fe5a3a
Sabina Omarova <sabomar170@gmail.com>
2022-10-23 11:02:24 -0400

    Lab 03- spring boot

---
+++ lab-03-springboot/src/main/java/com/cydeo/Lab03SpringbootApplication.java
@@ -0,0 +1,26 @@
+package com.cydeo;
+
+import com.cydeo.config.AuthorConfig;
+import com.cydeo.service.RecipeService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Lab03SpringbootApplication {
+
+    public static void main(String[] args) {
+
+    ApplicationContext context = SpringApplication.run(Lab03SpringbootApplication.class, args);
+    RecipeService recipeService = context.getBean(RecipeService.class);
+        recipeService.prepareRecipe();
+
+    AuthorConfig author = context.getBean(AuthorConfig.class);
+
+        System.out.println("Here you are! Author information of the recipe");
+        System.out.println("Name " + author.getName() + " Surname " + author.getSurname()
+                + "\n" + "Phone Number "  + author.getPhone() + "\n" + "Social Media Links "
+                + author.getSocialMedias() + "\n" + "Email " + author.getEmail());
+}
+
+}
