78cc808cfe7a8c8dfe55d2323ec5f279ee4996c7
Sabina Omarova <sabomar170@gmail.com>
2022-11-13 16:02:07 -0500

    Lab-07-orm-queries

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/ProductRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/ProductRepository.java
@@ -1,14 +1,30 @@
 package com.cydeo.lab07ormqueries.repository;
 
+import com.cydeo.lab07ormqueries.entity.Category;
+import com.cydeo.lab07ormqueries.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
-public interface ProductRepository {
-    //Write a derived query to get top 3 product order by price desc
-    //Write a derived query to get product by specific name
-    //Write a derived query to get product by specific category
-    //Write a derived query to get count by price greater than specific amount
-    //Write a derived query to get all product by quantity greater than or equal specific count
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    //Write a derived query to get top 3 product order by price desc:
+    List<Product> findTop3ByOrderByPriceDesc();
+
+    //Write a derived query to get product by specific name:
+    Product findFirstByName(String name);
+
+    //Write a derived query to get product by specific category:
+    List<Product> findAllByCategoryListContaining(Category category);
+
+    //Write a derived query to get count by price greater than specific amount:
+    Integer countProductByPriceGreaterThan(BigDecimal price);
+
+    //Write a derived query to get all product by quantity greater than or equal specific count:
+
+
     //Write a native query to get all product by price greater than specific amount and quantity lower than specific count
     //Write a native query to get all product by specific categoryId
     //Write a native query to get all product by specific categoryId and price greater than specific amount
