945c02c40450a5b0b1f5828f4cc4a6ca25c8c4cb
Sabina Omarova <sabomar170@gmail.com>
2022-11-13 14:57:20 -0500

    Lab-07-orm-queries

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CustomerRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CustomerRepository.java
@@ -1,10 +1,20 @@
 package com.cydeo.lab07ormqueries.repository;
 
+import com.cydeo.lab07ormqueries.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CustomerRepository{
-    //Write a derived query to get all customer by id
-    //Write a JPQL query to get customer by email
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    //Write a derived query to get customer by id:
+    Optional<Customer> findById(Long id);// if there is no customer
+
+    //Write a JPQL query to get customer by email:
+    @Query("SELECT c FROM Customer c WHERE c.email=?1")
+    Customer retrieveByCustomerEmail(String email);
+
 
 }
