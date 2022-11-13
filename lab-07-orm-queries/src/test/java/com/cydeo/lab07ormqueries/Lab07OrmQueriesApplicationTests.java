90b1f75e0ff29ffd8c4fa3cf143fde9db6a685ae
Sabina Omarova <sabomar170@gmail.com>
2022-11-13 12:07:00 -0500

    Lab-07-orm-queries

---
+++ lab-07-orm-queries/src/test/java/com/cydeo/lab07ormqueries/Lab07OrmQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab07ormqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab07OrmQueriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
