90c4e0a50c306c4bc06ee31f6b2df2bd1f804aeb
Sabina Omarova <sabomar170@gmail.com>
2022-12-08 16:28:31 -0500

    Lab-08-rest

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/ProductService.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/ProductService.java
@@ -2,17 +2,26 @@ package com.cydeo.lab08rest.service;
 
 import com.cydeo.lab08rest.dto.ProductDTO;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductService {
+
     List<ProductDTO> retrieveListProduct();
+
     ProductDTO updateProduct(ProductDTO productDTO);
 
     ProductDTO createProduct(ProductDTO productDTO);
 
-    ProductDTO retrieveProductByCategoryAndPrice();
+    List<ProductDTO> retrieveProductByCategoryAndPrice(List<Long> categoryList, BigDecimal price);
 
     ProductDTO retrieveByName(String name);
 
-    List<ProductDTO> retrieveProductByTup3ProductByPrice();
+    List<ProductDTO> retrieveProductByTop3ProductByPrice();
+
+    Integer countProductByPrice(BigDecimal price);
+
+    List<ProductDTO>  retrieveProductByPriceAndQuantity(BigDecimal price, Integer quantity);
+
+    List<ProductDTO> retrieveByCategory(Long categoryId);
 }
--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/impl/ProductServiceImpl.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/impl/ProductServiceImpl.java
@@ -12,22 +12,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ProductServiceImpl  implements ProductService {
+public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final MapperUtil mapperUtil;
+
 
     public ProductServiceImpl(ProductRepository productRepository, MapperUtil mapperUtil) {
         this.productRepository = productRepository;
         this.mapperUtil = mapperUtil;
     }
 
-
     @Override
     public List<ProductDTO> retrieveListProduct() {
         return productRepository.findAll().stream()
                 .map(product->mapperUtil.convert(product, new ProductDTO())).collect(Collectors.toList());
-
     }
 
     @Override
@@ -43,8 +42,8 @@ public class ProductServiceImpl  implements ProductService {
     }
 
     @Override
-    public ProductDTO retrieveProductByCategoryAndPrice(List<Long> categoryList, BigDecimal price) {
-        return productRepository.retrieveProductListByCategory(categoryList, price).stream()
+    public List<ProductDTO> retrieveProductByCategoryAndPrice(List<Long> categoryList, BigDecimal price) {
+        return productRepository.retrieveProductListByCategory(categoryList,price).stream()
                 .map(product -> mapperUtil.convert(product, new ProductDTO())).collect(Collectors.toList());
     }
 
@@ -52,4 +51,32 @@ public class ProductServiceImpl  implements ProductService {
     public ProductDTO retrieveByName(String name) {
         return mapperUtil.convert(productRepository.findFirstByName(name), new ProductDTO());
     }
+
+    @Override
+    public List<ProductDTO> retrieveProductByTop3ProductByPrice() {
+        return productRepository.findTop3ByOrderByPriceDesc().stream()
+                .map(product -> mapperUtil.convert(product, new ProductDTO())).collect(Collectors.toList());
+    }
+
+    @Override
+    public Integer countProductByPrice(BigDecimal price) {
+        return productRepository.countProductByPriceGreaterThan(price);
+    }
+
+    @Override
+    public List<ProductDTO> retrieveProductByPriceAndQuantity(BigDecimal price, Integer quantity) {
+        return productRepository.retrieveProductListGreaterThanPriceAndLowerThanRemainingQuantity(price, quantity)
+                .stream()
+                .map(product -> mapperUtil.convert(product, new ProductDTO()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ProductDTO> retrieveByCategory(Long categoryId) {
+        return productRepository.retrieveProductListByCategory(categoryId)
+                .stream()
+                .map(product -> mapperUtil.convert(product, new ProductDTO()))
+                .collect(Collectors.toList());
+    }
+
 }
